21f2179ccc5aa5217d3e0564dcc2b9f14a10973a
Jan <muellerj@cs.uni-bonn.de>
2026-01-29 14:42:56 +0100

    Renames package to avoid conflict with original 3dgs diff-gaussian-rasterization package.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # GRAPHDECO research group, https://team.inria.fr/graphdeco
 # All rights reserved.
 #
-# This software is free for non-commercial, research and evaluation use 
+# This software is free for non-commercial, research and evaluation use
 # under the terms of the LICENSE.md file.
 #
 # For inquiries contact  george.drettakis@inria.fr
@@ -12,24 +12,32 @@
 from setuptools import setup
 from torch.utils.cpp_extension import CUDAExtension, BuildExtension
 import os
+
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="diff_gaussian_rasterization",
-    packages=['diff_gaussian_rasterization'],
+    name="diff_gaussian_rasterization_mcmc",
+    packages=["diff_gaussian_rasterization_mcmc"],
     ext_modules=[
         CUDAExtension(
-            name="diff_gaussian_rasterization._C",
+            name="diff_gaussian_rasterization_mcmc._C",
             sources=[
-            "cuda_rasterizer/rasterizer_impl.cu",
-            "cuda_rasterizer/forward.cu",
-            "cuda_rasterizer/backward.cu",
-            "cuda_rasterizer/utils.cu",
-            "rasterize_points.cu",
-            "ext.cpp"],
-            extra_compile_args={"nvcc": ["-I" + os.path.join(os.path.dirname(os.path.abspath(__file__)), "third_party/glm/")]})
-        ],
-    cmdclass={
-        'build_ext': BuildExtension
-    }
+                "cuda_rasterizer/rasterizer_impl.cu",
+                "cuda_rasterizer/forward.cu",
+                "cuda_rasterizer/backward.cu",
+                "cuda_rasterizer/utils.cu",
+                "rasterize_points.cu",
+                "ext.cpp",
+            ],
+            extra_compile_args={
+                "nvcc": [
+                    "-I"
+                    + os.path.join(
+                        os.path.dirname(os.path.abspath(__file__)), "third_party/glm/"
+                    )
+                ]
+            },
+        )
+    ],
+    cmdclass={"build_ext": BuildExtension},
 )
